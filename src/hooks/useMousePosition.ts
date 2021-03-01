24309f8b74b3217c6c9c7129172e056ac2886b8c
xffz <695996211@qq.com>
2021-03-01 20:17:21 +0800

    hook2

--- src/hooks/useMousePosition.ts
+++ src/hooks/useMousePosition.ts
@@ -1,18 +1,22 @@
-import {onMounted, onUnmounted, ref} from "vue";
+import {onMounted, onUnmounted, reactive,toRefs} from "vue";
 
 function useMousePosition(){
-    const x = ref(0)
-    const y = ref(0)
-    const listenXAndY = (e: MouseEvent) => {
-        x.value = e.pageX
-        y.value = e.pageY
-    }
+    const data = reactive({
+        x: 0,
+        y: 0,
+        updateMouse:(e: MouseEvent) => {
+            data.x = e.pageX
+            data.y = e.pageY
+        }
+    })
     onMounted(() => {
-        document.addEventListener('click',listenXAndY)
+        document.addEventListener('click',data.updateMouse)
     })
-    onUnmounted(() => {
-        document.removeEventListener('click',listenXAndY)
+    onUnmounted(()=>{
+        document.removeEventListener('click',data.updateMouse)
     })
-    return {x,y}
+    const refData = toRefs(data)
+    return { ...refData }
+
 }
 export default useMousePosition
